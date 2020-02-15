78d3c40b16f4c31bba07f22481db4a938fe9409b
MuskanSadana <muskansadana@gmail.com>
2020-02-15 14:20:15 +0530

    my project

---
+++ python_code.py
@@ -0,0 +1,6 @@
+print("Hello Github!"
+        import pandas as pd
+        import numpy as np
+        This project is used for new project in my course in Capstone.
+        print("Hello Capstone Project Course!")
+        )
